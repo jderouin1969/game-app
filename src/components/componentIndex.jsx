eefa77017d48984514a9175258780b40250e61a6
Jonathan Derouin <jonathandero1969@outlook.com>
2025-12-04 14:24:09 -0600

    Changed all dialogs

--- src/components/componentIndex.jsx
+++ src/components/componentIndex.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react-refresh/only-export-components */
-export {ControlPanel} from './control-panel/ControlPanel.jsx';
 export {NavBar} from './nav-bar/NavBar.jsx';
 export {NumberPanel} from './number-panel/NumberPanel.jsx';
 export {PuzzleBox} from './puzzle-box/PuzzleBox.jsx';
+export {ControlPanel} from './control-panel/ControlPanel.jsx';
 export {default as control} from './control-panel/controlHandles.jsx';
-export {DifficultyDialog, FinishedDialog, DirectionsDialog} from './dialogs/dialogs.jsx';
+
 export {StartDialog} from './dialogs/startDialog.jsx';
+export {DifficultyDialog} from './dialogs/difficultyDialog.jsx';
+export {FinishedDialog} from './dialogs/finishedDialog.jsx';
+export {DirectionsDialog} from './dialogs/directionsDialog.jsx';
